d44ffacf5591ffc5a06973339be564ac4f4798f4
Romeo <pribyllromeo@gmail.com>
2025-09-29 14:50:50 +0200

    Refactor frontend configuration for improved API integration and deployment

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://3.120.227.99",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
